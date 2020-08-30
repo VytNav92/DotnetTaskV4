2a49dd518e5d27b08dc8393a8bbb9f886ea68b1d
Vytenis <vytenis.navalinskas@gmail.com>
2020-08-30 19:18:27 +0300

    Created DotnetTaskV4 solution with console project

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace DotnetTaskV4
+{
+    class Program
+    {
+        static async Task Main()
+        {
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadLine();
+        }
+    }
+}
